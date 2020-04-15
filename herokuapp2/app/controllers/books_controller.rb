5752721fb98bf1463b605059932a2934d4712da6
vagrant <vagrant@localhost.localdomain>
2020-04-15 09:51:12 +0000

    [Start]4-8

---
+++ herokuapp2/app/controllers/books_controller.rb
@@ -0,0 +1,45 @@
+class BooksController < ApplicationController
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+    if @book.save
+      flash[:notice] ="Book was successfully created."
+  	  redirect_to books_path
+  	else
+  	  redirect_to books_path
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+
+  	book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
